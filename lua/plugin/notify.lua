cf11b8b3f3a239f506b27cd6f2db4dc39cc07cb6
yee <yee@zyee.me>
2023-05-22 22:46:17 +0800

    feat: go use vscode-go

--- lua/plugin/notify.lua
+++ lua/plugin/notify.lua
@@ -50,34 +50,13 @@ end
 -- Make sure to also have the snippet with the common helper functions in your config!
 
 dap.listeners.before["event_progressStart"]["progress-notifications"] = function(session, body)
-    local notif_data = notify.get_notif_data("dap", body.progressId)
-
-    local message = format_message(body.message, body.percentage)
-
-    notif_data.notification = vim.notify(message, "info", {
-        title = format_title(body.title, session.config.type),
-        icon = notify.spinner_frames[1],
-        timeout = false,
-        hide_from_history = false,
-    })
-
-    notif_data.notification.spinner = 1, notify.update_spinner("dap", body.progressId)
+    notify.output_notify(message, format_title(body.title, session.config.type), "begin", "dap", body.progressId, "info")
 end
 
 dap.listeners.before["event_progressUpdate"]["progress-notifications"] = function(session, body)
-    local notif_data = notify.get_notif_data("dap", body.progressId)
-    notif_data.notification = vim.notify(format_message(body.message, body.percentage), "info", {
-        replace = notif_data.notification,
-        hide_from_history = false,
-    })
+    notify.output_notify(format_message(body.message, body.percentage), nil, "report", "dap", body.progressId, "info")
 end
 
 dap.listeners.before["event_progressEnd"]["progress-notifications"] = function(session, body)
-    local notif_data = client_notifs["dap"][body.progressId]
-    notif_data.notification = vim.notify(body.message and format_message(body.message) or "Complete", "info", {
-        icon = "",
-        replace = notif_data.notification,
-        timeout = 3000,
-    })
-    notif_data.spinner = nil
+    notify.output_notify(body.message and format_message(body.message) or "Complete", nil, "end", "dap", body.progressId, "info")
 end
